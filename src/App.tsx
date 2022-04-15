00d82d44a86554140b28fccc0b26776dad27827f
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2022-04-15 17:19:09 +0100

    Added Search and search styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
-
+import { Input } from './Components/Input/Input';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Navbar />
+      <Input />
     </div>
   )
 }
---
+++ src/Components/Input/Input.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import search from "../../assets/icon-search.svg";
+
+export const Input: React.FC = () => {
+    const [country, setCountry] = useState('');
+  return (
+    <main>
+      <form className="search">
+      <label className="search__image" htmlFor="search">
+          <img src={search} alt="" />
+        </label>
+        <input
+          required
+          className="search__input"
+          type="text"
+          name="searchgituser"
+          id="searchgituser"
+          placeholder="Search for a country..."
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        />
+      </form>
+    </main>
+  );
+};
