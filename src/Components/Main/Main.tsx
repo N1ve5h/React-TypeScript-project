26d0982d95e0127c531ff5c124aafd872af25c2a
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2022-04-16 01:29:57 +0100

    Added region to filter with mobile styling

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import search from "../../assets/icon-search.svg";
 
 export const Main: React.FC = () => {
-    const [country, setCountry] = useState('');
+  const [country, setCountry] = useState("");
+  const [region, setRegion] = useState("");
+
   return (
     <main>
       <form className="search">
-      <label className="search__image" htmlFor="search">
+        <label className="search__image" htmlFor="search">
           <img src={search} alt="" />
         </label>
         <input
@@ -20,6 +22,17 @@ export const Main: React.FC = () => {
           onChange={(e) => setCountry(e.target.value)}
         />
       </form>
+
+      <select name="region" className="regionFilter">
+        <option value="">Filter by Region</option>
+        <option value="africa">Africa</option>
+        <option value="americas">Americas</option>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
+      </select>
+
+      {/* <Country country={country}  region={region}/> */}
     </main>
   );
 };
