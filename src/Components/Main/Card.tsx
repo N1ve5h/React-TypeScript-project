e2681ad68a1c3e8cd9317e349b7afabd0fa7445e
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2022-04-22 10:50:28 +0100

    convert number to string

--- src/Components/Main/Card.tsx
+++ src/Components/Main/Card.tsx
@@ -19,7 +19,7 @@ export const Card: React.FC<Props> = ({country}) => {
         <div className="card">
             <img src={country['flags']['svg']} alt="" />
             <h1 className="title">{country['name']}</h1>
-            <p><b>Population: </b>{country['population']}</p>
+            <p><b>Population: </b>{country['population'].toLocaleString()}</p>
             <p><b>Region: </b>{country['region']}</p>
             <p><b>Capital: </b>{country['capital']}</p>
         </div>
