1b8ffde2dd90bfbbd3d2d68bccfeb237c85affef
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2022-04-27 12:54:26 +0100

    react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import './App.css';
-import { Navbar } from './Components/Navbar/Navbar';
-import { Main } from './Components/Main/Main';
+import React from "react";
+import "./App.css";
+import { Navbar } from "./Components/Navbar/Navbar";
+import { Main } from "./Components/Main/Main";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <Main />
-    </div>
-  )
-}
+    <Router>
+      <div className="App">
+        <Route path="/" element={<Navbar/>} />
+        <Route path="/" element={<Main/>} />
+      </div>
+    </Router>
+  );
+};
 export default App;
