dafd40b802eb2db4ca7988efafc2f7cb42c16821
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2022-04-27 13:39:04 +0100

    main page routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./App.css";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { Main } from "./Components/Main/Main";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
-        <Route path="/" element={<Navbar/>} />
-        <Route path="/" element={<Main/>} />
+        <Routes>
+          <Route path="/" element={<><Navbar /><Main /></>} />
+        </Routes>
       </div>
     </Router>
   );
