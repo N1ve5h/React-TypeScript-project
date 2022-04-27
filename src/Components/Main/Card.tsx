50e759a3f7a1f0032271bccbc12eb11c26d16b6a
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2022-04-27 22:23:11 +0100

    props separate file

--- src/Components/Main/Card.tsx
+++ src/Components/Main/Card.tsx
@@ -1,27 +1,13 @@
-interface Props {
-    country: {
-        name: String;
-        nativeName: String;
-        population: number;
-        region: String;
-        subregion: String;
-        capital: String;
-        topLevelDomain: String;
-        currencies: String;
-        languages: Array<String>;
-        borders: Array<String>;
-        flags: {svg: string};
-    }
-}
+import Props from "../../Props"
 
 export const Card: React.FC<Props> = ({country}) => {
     return (
         <div className="card">
-            <img src={country['flags']['svg']} alt="" />
-            <h1 className="title">{country['name']}</h1>
-            <p><b>Population: </b>{country['population'].toLocaleString()}</p>
-            <p><b>Region: </b>{country['region']}</p>
-            <p><b>Capital: </b>{country['capital']}</p>
+            <img src={country.flags.svg} alt="" />
+            <h1 className="title">{country.name}</h1>
+            <p><b>Population: </b>{country.population.toLocaleString()}</p>
+            <p><b>Region: </b>{country.region}</p>
+            <p><b>Capital: </b>{country.capital}</p>
         </div>
     )
 }
