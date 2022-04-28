e98061a0741ccbb129f5e6843339f609a2986dea
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2022-04-28 09:29:39 +0100

    Link

--- src/Components/Main/Card.tsx
+++ src/Components/Main/Card.tsx
@@ -1,27 +1,40 @@
+import { Link } from "react-router-dom";
+
 interface Props {
-    country: {
-        name: String;
-        nativeName: String;
-        population: number;
-        region: String;
-        subregion: String;
-        capital: String;
-        topLevelDomain: String;
-        currencies: String;
-        languages: Array<String>;
-        borders: Array<String>;
-        flags: {svg: string};
-    }
+  country: {
+    name: String;
+    nativeName: String;
+    population: number;
+    region: String;
+    subregion: String;
+    capital: String;
+    topLevelDomain: String;
+    currencies: String;
+    languages: Array<String>;
+    borders: Array<String>;
+    flags: { svg: string };
+  };
 }
 
-export const Card: React.FC<Props> = ({country}) => {
-    return (
-        <div className="card">
-            <img src={country['flags']['svg']} alt="" />
-            <h1 className="title">{country['name']}</h1>
-            <p><b>Population: </b>{country['population'].toLocaleString()}</p>
-            <p><b>Region: </b>{country['region']}</p>
-            <p><b>Capital: </b>{country['capital']}</p>
-        </div>
-    )
-}
+export const Card: React.FC<Props> = ({ country }) => {
+  return (
+    <Link>
+      <div className="card">
+        <img src={country["flags"]["svg"]} alt="" />
+        <h1 className="title">{country["name"]}</h1>
+        <p>
+          <b>Population: </b>
+          {country["population"].toLocaleString()}
+        </p>
+        <p>
+          <b>Region: </b>
+          {country["region"]}
+        </p>
+        <p>
+          <b>Capital: </b>
+          {country["capital"]}
+        </p>
+      </div>
+    </Link>
+  );
+};
