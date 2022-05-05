d71e8a9677413840bfd2c7156f12065c8c6affc3
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2022-05-05 12:00:48 +0100

    Details page

--- src/Components/Main/Country/Country.tsx
+++ src/Components/Main/Country/Country.tsx
@@ -1,10 +1,51 @@
+import { useLocation } from "react-router";
+import arrow from "../../../assets/arrow-left-long-solid.svg";
+import { Link } from "react-router-dom";
+
 export const Country: React.FC = () => {
-    let country = location.param;
-    return (
-       
-      <div>
-          
-          <h1>Hello</h1>
+  let location = useLocation();
+  let { country }: any = location.state;
+  
+  return (
+    <>
+      <Link to='/'><button className="backButton">
+        <img src={arrow} /> Back
+      </button>
+      </Link>
+      <div className="card detail">
+        <img src={country.flags.svg} alt="" />
+        <h1 className="title">{country.name}</h1>
+        <p>
+          <b>Population: </b>
+          {country.population.toLocaleString()}
+        </p>
+        <p>
+          <b>Region: </b>
+          {country.region}
+        </p>
+        <p>
+          <b>Sub Region: </b>
+          {country.subregion}
+        </p>
+        <p>
+          <b>Capital: </b>
+          {country.capital}
+        </p>
       </div>
-    );
-  };
+      <div className="card detail second">
+        <p>
+          <b>Top Level Domain: </b>
+          {country.topLevelDomain}
+        </p>
+        <p>
+          <b>Currencies: </b>
+          {country.currencies[0].name}
+        </p>
+        <p>
+          <b>Languages:</b>
+          {country.languages.map((language: { name: string; }) => ' '+language.name)}
+        </p>
+      </div>
+    </>
+  );
+};
