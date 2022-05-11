f273e8b2f2d64db0cc0980448f48e919a255094d
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2022-05-11 11:25:51 +0100

    link

--- src/Components/Main/Card.tsx
+++ src/Components/Main/Card.tsx
@@ -1,16 +1,9 @@
 import { Link } from "react-router-dom";
 import Props from "../../Props";
-import { Country } from "./Country/Country";
 
-<<<<<<< HEAD
 export const Card: React.FC<Props> = ({ country }) => {  
   return (
     <Link to={`/country/${country.name}`} state={{country}}>
-=======
-export const Card: React.FC<Props> = ({ country }) => {
-  return (
-    <Link to={{pathname:`/country/${country.name}`}} state={country}>
->>>>>>> 68cf064610766718e221796f827974b95a7e2e2f
       <div className="card">
         <img src={country.flags.svg} alt="" />
         <h1 className="title">{country.name}</h1>
