c913c880b9ef8966ceaeaa9529d7d89ecb2f3cbc
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2022-11-22 22:45:19 +0000

    able to style using multiple classe

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 import { NavItem } from "./NavItem";
 import night from "../../assets/icon-moon.svg";
 import day from "../../assets/icon-sun.svg";
+import styles from "./Navbar.module.css";
+
+let setClass = (classes: string[]) => {
+  return classes.map((className) => styles[className]).join(" ");
+}
 
 export const Navbar: React.FC = () => {
   const [theme, setTheme] = useState("light");
@@ -10,11 +15,11 @@ export const Navbar: React.FC = () => {
   const themeToggler = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
   };
-
+  
   return (
-    <header className="navbar">
-      <h1>Where in the world?</h1>
-      <section className="colorButton" onClick={() => themeToggler()}>
+    <header className={setClass(["flex", "row"])}>
+      <h1 className={styles.heading}>Where in the world?</h1>
+      <section className={styles.flex} onClick={() => themeToggler()}>
         {theme === "light" ? <NavItem img={night} title={"Dark"}/> :  <NavItem img={day} title={"Light"}/>}
       </section>
     </header>
