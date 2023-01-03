16472a718acefd1362beb815df619d634044b1b5
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2023-01-03 20:31:05 +0000

    Dark mode toggle functionality implemented

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { useState } from "react";
-import { NavItem } from "./NavItem";
+import { ButtonTheme } from "./ButtonTheme";
 import night from "../../assets/icon-moon.svg";
 import day from "../../assets/icon-sun.svg";
 import styles from "./Navbar.module.css";
 
-let setClass = (classes: string[]) => {
-  return classes.map((className) => styles[className]).join(" ");
-};
-
 export const Navbar: React.FC = () => {
   const [theme, setTheme] = useState("light");
-
   const themeToggler = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
   };
@@ -20,13 +15,22 @@ export const Navbar: React.FC = () => {
     <header>
       <nav className={styles.navbar}>
         <h1 className={styles.heading}>Where in the world?</h1>
-        <button className={styles.button} onClick={() => themeToggler()}>
-          {theme === "light" ? (
-            <NavItem img={night} title={"Dark"} />
-          ) : (
-            <NavItem img={day} title={"Light"} />
-          )}
-        </button>
+
+        {theme === "light" ? (
+          <button
+            className={`${styles.button} ${styles.button__light}`}
+            onClick={() => themeToggler()}
+          >
+            <ButtonTheme img={night} title={"Dark"} />
+          </button>
+        ) : (
+          <button
+            className={`${styles.button} ${styles.button__dark}`}
+            onClick={() => themeToggler()}
+          >
+            <ButtonTheme img={day} title={"Light"} />
+          </button>
+        )}
       </nav>
     </header>
   );
