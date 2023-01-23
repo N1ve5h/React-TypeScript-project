94cae6b9b2b8c24a1fa3842d45939d5483ac1024
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2023-01-23 14:27:21 +0000

    passing themetoggler function through props to assign theme across the whole app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./reset.css";
 import "./body.css";
 import { Navbar } from "./Components/Navbar/Navbar";
@@ -7,10 +8,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Country } from "./Components/Country/Country";
 
 const App: React.FC = () => {
+  const [darkTheme, setDarkTheme] = useState(true);
+  
+  const themeToggler = () => {
+    setDarkTheme(!darkTheme);
+  };
+
   return (
     <Router>
       <div className="App">
-      <Navbar />
+      <Navbar darkTheme={darkTheme} themeToggler={themeToggler}/>
         <Routes>   
           <Route path="/" element={<Main />} />
           <Route path="/country/:name" element={<Country/>} />
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,32 +1,32 @@
-import React from "react";
-import { useState } from "react";
+import React, { MouseEventHandler } from "react";
 import { ButtonTheme } from "./ButtonTheme";
 import night from "../../assets/icon-moon.svg";
 import day from "../../assets/icon-sun.svg";
 import styles from "./Navbar.module.css";
 
-export const Navbar: React.FC = () => {
-  const [theme, setTheme] = useState("light");
-  const themeToggler = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  };
+type ThemeProps = {
+  darkTheme: boolean;
+  themeToggler: MouseEventHandler;
+};
+
+export const Navbar: React.FunctionComponent<ThemeProps> = (props) => {
+  const { darkTheme, themeToggler } = props;
 
   return (
-    <header>
+    <header className={darkTheme ? "dark" : "light"}>
       <nav className={styles.navbar}>
         <h1 className={styles.heading}>Where in the world?</h1>
-
-        {theme === "light" ? (
+        {!darkTheme ? (
           <button
             className={`${styles.button} ${styles.button__light}`}
-            onClick={() => themeToggler()}
+            onClick={themeToggler}
           >
             <ButtonTheme img={night} title={"Dark"} />
           </button>
         ) : (
           <button
             className={`${styles.button} ${styles.button__dark}`}
-            onClick={() => themeToggler()}
+            onClick={themeToggler}
           >
             <ButtonTheme img={day} title={"Light"} />
           </button>
